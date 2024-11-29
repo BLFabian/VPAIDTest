fcd3b5e7a0d683a43557cb58ca7d921bd9c8d874
fabian-bjorne-larsen <fabian.bjorne-larsen@schibsted.com>
2024-11-29 10:57:06 +0100

    Update vpaid2.js

--- vpaid2.js
+++ vpaid2.js
@@ -1,46 +1,87 @@
-  (function () {
-    var adCreative = {
-      initAd: function (width, height, viewMode, desiredBitrate, creativeData, environmentVars) {
-        var adContainer = environmentVars.slot;
-        var blueBox = document.createElement('div');
-        blueBox.style.width = '100%';
-        blueBox.style.height = '100%';
-        blueBox.style.backgroundColor = 'blue';
-        blueBox.style.display = 'flex';
-        blueBox.style.alignItems = 'center';
-        blueBox.style.justifyContent = 'center';
-        blueBox.style.color = 'white';
-        blueBox.style.fontSize = '48px';
-        blueBox.innerText = 'HI';
-        adContainer.appendChild(blueBox);
-      },
-      startAd: function () {
-        console.log('Ad started');
-      },
-      stopAd: function () {
-        console.log('Ad stopped');
-      },
-      skipAd: function () {
-        console.log('Ad skipped');
-      },
-      resizeAd: function (width, height, viewMode) {
-        console.log('Ad resized');
-      },
-      pauseAd: function () {
-        console.log('Ad paused');
-      },
-      resumeAd: function () {
-        console.log('Ad resumed');
-      },
-      expandAd: function () {
-        console.log('Ad expanded');
-      },
-      collapseAd: function () {
-        console.log('Ad collapsed');
-      },
-    };
+(function () {
+  function VPAIDAd() {
+    this.slot = null;
+    this.videoSlot = null;
+    this.videoSlotCanAutoPlay = false;
+    this.eventsCallbacks = {};
+  }
 
-    window.getVPAIDAd = function () {
-      return adCreative;
-    };
-  })();
+  VPAIDAd.prototype.initAd = function (
+    width,
+    height,
+    viewMode,
+    desiredBitrate,
+    creativeData,
+    environmentVars
+  ) {
+    // Environment vars
+    this.slot = environmentVars.slot;
+    this.videoSlot = environmentVars.videoSlot;
+
+    // Create the blue box
+    var blueBox = document.createElement("div");
+    blueBox.style.width = width + "px";
+    blueBox.style.height = height + "px";
+    blueBox.style.backgroundColor = "blue";
+    blueBox.style.display = "flex";
+    blueBox.style.alignItems = "center";
+    blueBox.style.justifyContent = "center";
+    blueBox.style.color = "white";
+    blueBox.style.fontSize = "48px";
+    blueBox.innerText = "HI";
+
+    // Add the blue box to the container
+    if (this.slot) {
+      this.slot.appendChild(blueBox);
+    }
+
+    // Notify that the ad has loaded
+    this.callEvent("AdLoaded");
+  };
+
+  VPAIDAd.prototype.startAd = function () {
+    this.callEvent("AdStarted");
+  };
+
+  VPAIDAd.prototype.stopAd = function () {
+    this.callEvent("AdStopped");
+  };
+
+  VPAIDAd.prototype.resizeAd = function (width, height, viewMode) {
+    console.log("Ad resized to " + width + "x" + height);
+  };
+
+  VPAIDAd.prototype.pauseAd = function () {
+    this.callEvent("AdPaused");
+  };
+
+  VPAIDAd.prototype.resumeAd = function () {
+    this.callEvent("AdPlaying");
+  };
+
+  VPAIDAd.prototype.expandAd = function () {
+    this.callEvent("AdExpanded");
+  };
+
+  VPAIDAd.prototype.collapseAd = function () {
+    this.callEvent("AdCollapsed");
+  };
+
+  VPAIDAd.prototype.subscribe = function (callback, eventName) {
+    this.eventsCallbacks[eventName] = callback;
+  };
+
+  VPAIDAd.prototype.unsubscribe = function (eventName) {
+    delete this.eventsCallbacks[eventName];
+  };
+
+  VPAIDAd.prototype.callEvent = function (eventType) {
+    if (this.eventsCallbacks[eventType]) {
+      this.eventsCallbacks[eventType]();
+    }
+  };
+
+  window.getVPAIDAd = function () {
+    return new VPAIDAd();
+  };
+})();
